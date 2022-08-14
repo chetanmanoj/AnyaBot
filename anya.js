2761e48d5b72f00ac2c22ce186df04ecb1147c7b
Chetan Manoj <chetanmanoj@Chetans-MacBook-Air.local>
2022-08-14 16:46:06 +0530

    Added help command and command categories

--- anya.js
+++ anya.js
@@ -1,28 +1,38 @@
-console.log('hello! i am anya forger!');
-const Discord = require('discord.js');
+console.log("Hello! I am Anya F-F-F-Forger!");
+const Discord = require("discord.js");
 require("dotenv").config();
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 client.login(process.env.TOKEN);
 
-client.on('ready', readydisc);
+client.on("ready", readydisc);
 function readydisc() {
-    console.log("Ready to go!");
+  console.log("Ready to go!");
 }
 
-client.on('message', gotMessage);
+client.on("message", gotMessage);
 function gotMessage(msg) {
-    console.log(msg.content);
-    if (msg.content === '!name') {
-        msg.reply('I am Anya F-F-F-Fo-Forger...');
-    }
-    if (msg.content === '!address') {
-        msg.reply("Anya's home!");
-    }
-    if (msg.content === '!glen') {
-        msg.reply('GLen is a panda');
-    }
-    if (msg.content === '!gif') {
+  console.log(msg.content);
+  if (msg.content === "a.help") {
+    msg.reply(
+      " **Anya is a good helper!** \nCategories: \n``anime``: Anime commands \n``misc``: Miscellaneous commands"
+    );
+  }
+  if (msg.content === "a.help misc") {
+    msg.reply(
+      "``a.name``: Find out my name🦧 \n``a.address``: Where do I live \n``a.glen``: Find out some facts about Glen"
+    );
+  }
 
-    }
+  if (msg.content === "a.name") {
+    msg.reply("I am Anya F-F-F-Fo-Forger...");
+  }
+  if (msg.content === "a.address") {
+    msg.reply("Anya's home!");
+  }
+  if (msg.content === "a.glen") {
+    msg.reply("GLen is a panda");
+  }
+  if (msg.content === "!gif") {
+  }
 }
