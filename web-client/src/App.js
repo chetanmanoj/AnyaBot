e25b25dbbf710381a394e9ca09110701e3e2eb45
glenpereira <glenv.pereira@gmail.com>
2022-08-17 18:23:35 +0530

    added placeholder website

---
+++ web-client/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+        <a href = "https://myanimelist.net/character/170256/Anya_Forger" >
+            <h1 className="title">Waku Waku!</h1>
+        </a>
+    </div>
+  );
+}
+
+export default App;
