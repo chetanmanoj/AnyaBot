4e4a1c4e19ff3272e5d7eb6fe4390b41ffb0aa8c
chetanmanoj <chetanmanoj2001@gmail.com>
2022-10-31 22:27:15 +0530

    WIP: Implementing puppeteer

--- src/anya.js
+++ src/anya.js
@@ -1,6 +1,14 @@
 console.log("Hello! I am Anya F-F-F-Forger!");
 const Discord = require("discord.js");
+const puppeteer = require("puppeteer");
 require("dotenv").config();
+
+async function anime(){
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto("https://subsplease.org/")
+
+}
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 client.login(process.env.TOKEN);
@@ -11,31 +19,38 @@ function readydisc() {
 }
 
 client.on("message", gotMessage);
-function gotMessage(msg) {
+async function gotMessage(msg) {
   console.log(msg.content);
+
   const mainCommand =
     " **Anya is a good helper!** \nCategories: \n``anime``: Anime commands \n``misc``: Miscellaneous commands \n\n To know the available commands for each category, use  ``a.help <category>``";
-  if (msg.content === "a.help") {
+  let aniName;
+  let tokens = msg.content.split(' '); 
+
+ if (tokens[0] === "a.help") {
     msg.reply(mainCommand);
   }
-  if (msg.content === "a.help misc") {
+  else if (tokens[0] === "a.help misc") {
     msg.reply(
       "``a.name``: Find out my name🦧 \n``a.address``: Where do I live \n``a.glen``: Find out some facts about Glen"
     );
   }
-  if (msg.content === "a.name") {
+  else if (tokens[0] === "a.name") {
     msg.reply("I am Anya F-F-F-Fo-Forger...");
   }
-  if (msg.content === "a.address") {
+  else if (tokens[0] === "a.address") {
     msg.reply("Anya's home!");
   }
-  if (msg.content === "a.glen") {
-    msg.reply("GLen is a pandaboi");
-  }
-  if (msg.content === "anya") {
+  else if (tokens[0] === "anya") {
     msg.reply("Waku Waku!");
   }
-  // if (msg.content === "a.warn") {
-  //   msg.reply("Be good! or Anya throw peanuts at you!");
-  // }
+  else if (tokens[0] === "a.anime"){
+    let keywords = "boobs";
+    if(tokens.length>1){
+      keywords = tokens.slice(1, tokens.length).join(" ");
+    }
+    msg.reply(keywords);
+  }
+
+  
 }
